6359c9093fe1354f6e36334488a8629946b7fd59
RanganathanApps <apps.ranganathan@gmail.com>
2019-08-17 17:17:43 +0530

    minor update

--- app/src/main/java/com/apps/officialbase/MainActivity.kt
+++ app/src/main/java/com/apps/officialbase/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.apps.officialbase
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
 import com.apps.baselibrary.BaseActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,24 +12,21 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            makePostRequest("")
+
+        fab.setOnClickListener {
+            makePostRequest("https://fierce-cove-29863.herokuapp.com/createAnUser1",object :ApiResponse{
+                override fun onError(error: Any) {
+
+                    showToast(this@MainActivity,error.toString())
+                }
+
+                override fun onCompleted(res: Any) {
+                    showToast(this@MainActivity,res.toString())
+                }
+
+            })
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
--- baselibrary/src/main/java/com/apps/baselibrary/BaseActivity.kt
+++ baselibrary/src/main/java/com/apps/baselibrary/BaseActivity.kt
@@ -1,12 +1,9 @@
 package com.apps.baselibrary
 
-import android.os.Bundle
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
-import com.apps.baselibrary.R
 
-import kotlinx.android.synthetic.main.activity_base.*
 import com.androidnetworking.error.ANError
 import org.json.JSONObject
 import com.androidnetworking.interfaces.JSONObjectRequestListener
@@ -19,20 +16,35 @@ import com.androidnetworking.common.Priority
 
 open class BaseActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
-        setSupportActionBar(toolbar)
+    open lateinit var listener: ApiResponse
 
 
+    private fun setRequestListener():JSONObjectRequestListener{
+       return  object : JSONObjectRequestListener {
+            override fun onResponse(response: JSONObject) {
+                // do anything with response
+                makeLog("networking","success")
+                listener.onCompleted(response)
+            }
+
+            override fun onError(error: ANError) {
+                // handle error
+                makeLog("networking","error")
+                listener.onError(error)
+            }
+        }
     }
 
-    internal fun showSnackbar(view:View,msg:String){
+    public fun setListeners(res:ApiResponse){
+        listener = res
+    }
+
+    protected fun showSnackbar(view:View,msg:String){
         Snackbar.make(view, msg, Snackbar.LENGTH_LONG)
             .setAction("Action", null).show()
     }
 
-    internal fun showTost(context:Context,msg:String){
+    protected fun showToast(context:Context,msg:String){
         Toast.makeText( context,msg, Toast.LENGTH_LONG).show()
     }
 
@@ -49,25 +61,22 @@ open class BaseActivity : AppCompatActivity() {
     }
 
 
-    protected fun makePostRequest(msg:String){
-        AndroidNetworking.post("https://fierce-cove-29863.herokuapp.com/createAnUser")
+    protected fun makePostRequest(url: String, param: ApiResponse){
+        setListeners(param)
+        AndroidNetworking.post(url)
             .addBodyParameter("firstname", "Amit")
             .addBodyParameter("lastname", "Shekhar")
             .setTag("test")
             .setPriority(Priority.MEDIUM)
             .build()
-            .getAsJSONObject(object : JSONObjectRequestListener {
-                override fun onResponse(response: JSONObject) {
-                    // do anything with response
-                    makeLog("networking","success")
-                }
-
-                override fun onError(error: ANError) {
-                    // handle error
-                    makeLog("networking","error")
-                }
-            })
+            .getAsJSONObject(setRequestListener())
 
     }
+
+    interface ApiResponse{
+        public fun onCompleted(res:Any)
+        public fun onError(error:Any)
+    }
+
 
 }
